972a7e3e91f0e139c92cb8841106ce91e6863222
Noah Perry <noah@perrybrain.com>
2022-03-08 15:09:18 -0600

    updates

--- README.md
+++ README.md
@@ -4,9 +4,32 @@ Tehc Site Engine is a website engine to create dynamic webpages from multiple HT
 
 ## Usage
 
+HTML:
+
 ```html
-...
+<!DOCTYPE html>
+<html>
 <head>
     ...
-    <script src="">
+    <script src="https://cdn.jsdelivr.net/gh/Tehcn/TehcSiteEngine@main/src/engine.js">
+    ...
+</head>
+<body>
+    ...
+    <main id="root">
+
+    </main>
+    ...
+</body>
+</html>
+```
+
+File Structure:
+
+```txt
+(root)
+|   index.html
+\---views
+        home.html
+        test.html
 ```
--- src/engine.js
+++ src/engine.js
@@ -1,13 +1,11 @@
-window.onload = function() {
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, props) => searchParams.get(props),
-    });
-    
-    let page = params.page;
-    
-    (async () => {
-        if(page == null || page == '') page = 'home';
-        document.getElementById('root').innerHTML = await (await fetch(`/views?page=${page}`)).text();
-        document.querySelector('title').innerText = `${page.charAt(0).toUpperCase() + page.slice(1)} - ${document.querySelector('title').innerText}`;
-    })();
-}
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, props) => searchParams.get(props),
+});
+
+let page = params.page;
+
+(async () => {
+    if(page == null || page == '' || page == undefined) page = 'home';
+    document.getElementById('root').innerHTML = await (await fetch(`/views?page=${page}`)).text();
+    document.querySelector('title').innerText = `${page.charAt(0).toUpperCase() + page.slice(1)} - ${document.querySelector('title').innerText}`;
+})();
